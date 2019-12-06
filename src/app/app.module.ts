a9789970f7e65c4c786c1c5709a3d33d55468fd2
Ladner, Denis <Denis.Ladner@EXXETA.com>
2019-12-06 16:16:08 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CsvViewerComponent } from './csv-viewer/csv-viewer.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { DataTableComponent } from './data-table/data-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CsvViewerComponent,
+    PaginationComponent,
+    DataTableComponent
   ],
   imports: [
     BrowserModule
---
+++ src/personen.ts
@@ -0,0 +1,201 @@
+export const personen = 'Name;Vorname;Strasse;Ort;Alter\n' +
+  'Yang;Paloma;P.O. Box 451, 8598 Donec Road;Bell;88\n' +
+  'Diaz;Jayme;Ap #486-1122 Vestibulum Road;Lafayette;45\n' +
+  'Valencia;Kelly;755-2020 Erat. Rd.;Lincoln;60\n' +
+  'Lindsay;Knox;Ap #732-8577 Pharetra Ave;West Sacramento;68\n' +
+  'Donovan;Rowan;P.O. Box 356, 2474 Pede. St.;Jacksonville;75\n' +
+  'Nielsen;Keane;851-1918 Morbi St.;Wilmington;12\n' +
+  'Underwood;Cassidy;Ap #267-5405 Faucibus Rd.;La Palma;67\n' +
+  'Dickerson;Finn;Ap #563-8001 Est. Avenue;San Bernardino;52\n' +
+  'Bonner;Alexa;Ap #566-2220 Eget Avenue;St. George;87\n' +
+  'Bates;Amal;1158 Vulputate, St.;Santa Monica;48\n' +
+  'Sanders;Zephania;Ap #370-6019 Habitant Street;Jeffersonville;26\n' +
+  'Chapman;Vladimir;Ap #248-9755 Aliquam Avenue;Logan;8\n' +
+  'Nielsen;Alec;3483 Nisl Rd.;Milford;83\n' +
+  'Larsen;Kaseem;P.O. Box 855, 3312 Convallis Street;Brea;11\n' +
+  'Beck;Hollee;Ap #891-4333 Vel Rd.;Saginaw;75\n' +
+  'Walters;Victoria;P.O. Box 526, 4759 Ultricies Rd.;Frankfort;90\n' +
+  'Huff;Mary;P.O. Box 175, 4057 Dignissim Road;Littleton;12\n' +
+  'Dickerson;Sage;6454 Mollis. Road;Beckley;34\n' +
+  'Gay;Wayne;7379 Erat St.;Attleboro;53\n' +
+  'Cline;Anthony;153-1152 Quam Rd.;Ames;57\n' +
+  'Johnston;Chava;Ap #853-1272 Pretium Av.;Lynchburg;7\n' +
+  'Jenkins;Hermione;556 Arcu Rd.;Leominster;51\n' +
+  'Carter;Dorian;4846 Justo St.;East Rutherford;83\n' +
+  'Calderon;Macon;Ap #643-7206 Neque Street;Shawnee;29\n' +
+  'Mcintosh;Kasper;P.O. Box 298, 1978 Leo Ave;Kahului;85\n' +
+  'Porter;Clayton;1403 Non, Street;Little Rock;6\n' +
+  'Garcia;Gloria;709-2060 Luctus. Rd.;Eatontown;83\n' +
+  'Shaw;Silas;P.O. Box 307, 1927 Sapien. Rd.;Gaithersburg;86\n' +
+  'Daugherty;Kermit;882-8683 Integer Rd.;Knoxville;9\n' +
+  'Moody;Len;9568 Nullam Ave;Durango;45\n' +
+  'Deleon;Ursa;5302 Aliquam Rd.;Grass Valley;43\n' +
+  'Collins;Katell;6552 Est Road;Muskegon;35\n' +
+  'Rojas;Denise;P.O. Box 383, 7571 Egestas Rd.;Rohnert Park;82\n' +
+  'Good;Jane;Ap #376-5470 Vulputate Street;Normal;90\n' +
+  'Ball;Jermaine;7147 Nascetur Road;Tucson;60\n' +
+  'Bowen;Patricia;P.O. Box 187, 9421 Auctor Ave;Worcester;68\n' +
+  'Berger;Eric;P.O. Box 875, 2974 Nibh Av.;Anaheim;16\n' +
+  'Acevedo;Frances;770-1237 Nunc Rd.;South Burlington;12\n' +
+  'Berger;Kuame;Ap #796-421 Pede Ave;Gary;82\n' +
+  'Cabrera;Dominique;271-3995 Et Av.;Chicopee;36\n' +
+  'Duffy;Jade;770-1457 Integer Road;Bloomington;7\n' +
+  'Strong;Cleo;526-465 At St.;Yuma;11\n' +
+  'Sampson;Alice;P.O. Box 745, 6038 Amet, Ave;Auburn;5\n' +
+  'Roman;Iona;Ap #705-6063 Non St.;Brunswick;12\n' +
+  'Golden;Deirdre;P.O. Box 486, 3387 Nonummy. Road;Cedar Falls;64\n' +
+  'Ryan;Chaney;7360 Lacinia Avenue;Hastings;64\n' +
+  'Tate;Barry;P.O. Box 184, 3815 Porttitor Avenue;Hagerstown;26\n' +
+  'Bernard;Allen;P.O. Box 306, 4457 Ultricies St.;Hickory;59\n' +
+  'Mayo;Rosalyn;Ap #805-9033 Leo. Ave;Davis;64\n' +
+  'Taylor;Willa;1799 Aliquam Rd.;Troy;71\n' +
+  'Fry;Zephania;306-7164 Eu Ave;Chattanooga;38\n' +
+  'Stokes;Harriet;P.O. Box 488, 862 Cursus Street;Akron;14\n' +
+  'Mcgowan;Rebekah;953-7745 Eu, Av.;Montgomery;50\n' +
+  'Merrill;Philip;P.O. Box 496, 5719 Sit Road;Rhinelander;65\n' +
+  'Ross;James;329-754 Sit St.;Nacogdoches;26\n' +
+  'Key;Miriam;174-7070 Auctor Rd.;Birmingham;53\n' +
+  'Mueller;Slade;924-6493 Sem St.;Thibodaux;89\n' +
+  'Bowen;Jakeem;273-6748 Curae; Av.;La Verne;48\n' +
+  'Velazquez;Keith;P.O. Box 827, 5073 Semper. St.;Paramount;8\n' +
+  'Mcconnell;Harlan;P.O. Box 132, 3367 Vel, St.;Dixon;47\n' +
+  'Donovan;Sybil;8685 Eget Street;Westfield;70\n' +
+  'Burns;Camille;Ap #429-1286 Vel Rd.;Scottsdale;52\n' +
+  'Carroll;Maggie;Ap #698-2186 Tincidunt Av.;Taunton;75\n' +
+  'Donovan;Tyrone;466-3679 Eu Rd.;South Burlington;58\n' +
+  'Garrett;Noelle;7611 Faucibus St.;Martinsburg;39\n' +
+  'Horton;Thaddeus;722-8921 Eleifend Street;Michigan City;47\n' +
+  'Mckay;Cullen;P.O. Box 978, 4038 Ultrices Rd.;Danville;42\n' +
+  'Sharp;Magee;233-3989 Ut Ave;Stillwater;23\n' +
+  'Dawson;Magee;P.O. Box 718, 2007 Sit Rd.;Glendale;16\n' +
+  'Sullivan;Cynthia;9570 Posuere Road;Cape Girardeau;28\n' +
+  'Alvarado;Blythe;Ap #585-8597 Arcu Street;High Point;20\n' +
+  'Burt;Kelsey;8419 Ante Avenue;New Iberia;51\n' +
+  'Keller;Naida;P.O. Box 478, 5718 Dictum Ave;East Rutherford;14\n' +
+  'Wilkerson;Austin;209-2233 A, St.;Bloomington;32\n' +
+  'Rocha;Malik;3425 Elit. St.;Plano;42\n' +
+  'Gardner;Joel;P.O. Box 817, 6358 Lectus Avenue;Fort Collins;44\n' +
+  'Murray;Odysseus;286 Nunc Avenue;Wynne;89\n' +
+  'Burt;Halla;513-1316 Mollis Road;Bozeman;61\n' +
+  'Fletcher;Willa;401-4915 Lacus. Avenue;Pulaski;26\n' +
+  'Little;Kay;P.O. Box 610, 5672 Massa. Av.;Baldwin Park;43\n' +
+  'Holt;Asher;P.O. Box 858, 5314 Magna Road;Marlborough;29\n' +
+  'Woodard;Maya;3763 Vehicula Rd.;Orlando;86\n' +
+  'Romero;Abbot;976-526 Diam Rd.;Columbus;74\n' +
+  'Bass;Porter;P.O. Box 832, 1783 Ligula. St.;Pittsburgh;22\n' +
+  'Griffin;Kessie;7508 Tellus Avenue;Mesa;25\n' +
+  'Henderson;Justine;487-841 Quis, St.;Thousand Oaks;86\n' +
+  'Gordon;Sonya;Ap #136-3092 Laoreet Rd.;Bangor;13\n' +
+  'Little;Stacy;P.O. Box 706, 7836 Non, Street;Stillwater;52\n' +
+  'Moon;Lysandra;P.O. Box 670, 5503 Lorem Avenue;Bossier City;9\n' +
+  'Williamson;Cassidy;Ap #864-9205 In, Avenue;Beckley;48\n' +
+  'Wheeler;Hakeem;4590 In, St.;Idaho Springs;14\n' +
+  'Malone;Axel;Ap #608-507 Mattis Road;San Bernardino;43\n' +
+  'Bentley;Idona;Ap #994-3367 Non, St.;Lebanon;83\n' +
+  'Snider;Garrison;8030 Eu Avenue;Flint;41\n' +
+  'Tillman;Clayton;519-4000 Nunc Street;Fairfax;81\n' +
+  'Bauer;Shana;997-2627 Felis, Av.;Glendale;15\n' +
+  'Richard;Freya;407-6030 Neque. Street;Bloomington;24\n' +
+  'Jacobs;Tara;7722 Mauris Avenue;Shelton;10\n' +
+  'Burt;Emerson;P.O. Box 595, 2611 Suspendisse Road;Burlingame;35\n' +
+  'May;Brenda;465-2489 Eu Avenue;Rohnert Park;60\n' +
+  'Kemp;Bertha;758-4869 Torquent Rd.;Visalia;59\n' +
+  'Ewing;Neville;7840 Hendrerit Road;Taylorsville;21\n' +
+  'Wiley;Indigo;680 Hendrerit St.;Culver City;42\n' +
+  'Wilcox;Joan;P.O. Box 576, 7068 Et, St.;Grand Rapids;72\n' +
+  'Fitzpatrick;Nora;Ap #261-594 Vel Ave;Green Bay;60\n' +
+  'Holder;Chadwick;474-7485 Luctus Street;Tok;46\n' +
+  'Marsh;Whilemina;2126 Tincidunt St.;The Dalles;12\n' +
+  'Howell;Idola;P.O. Box 213, 5201 Elementum, Av.;Rancho Cucamonga;81\n' +
+  'Chang;Trevor;Ap #443-6957 Quis, Rd.;Ann Arbor;63\n' +
+  'Briggs;Irma;158 Tempor Avenue;Webster Groves;61\n' +
+  'Baird;Stacy;P.O. Box 391, 3426 Volutpat. Avenue;Simi Valley;12\n' +
+  'Lowery;Wynter;8041 Lacinia Street;Connellsville;74\n' +
+  'Fuentes;Quyn;8918 Mauris St.;Broken Arrow;22\n' +
+  'Gay;Blair;P.O. Box 869, 3740 Interdum. Road;Vernon;66\n' +
+  'Figueroa;Valentine;1830 Libero Rd.;Chula Vista;66\n' +
+  'Sanders;Ivory;Ap #464-9332 Imperdiet Rd.;Irving;54\n' +
+  'Ware;Colette;840-1706 Tempor, St.;North Adams;15\n' +
+  'Dale;Jonas;P.O. Box 833, 811 Molestie St.;Ephraim;40\n' +
+  'Shaw;Ivana;148-1655 Lobortis Rd.;Sutter Creek;63\n' +
+  'Marks;Risa;3352 Vulputate St.;Odessa;73\n' +
+  'Roberts;Jenna;Ap #267-9101 Aliquam Road;Chino;30\n' +
+  'Cabrera;Yolanda;315-8517 Vitae Road;Anaconda;73\n' +
+  'Bryant;Athena;5278 Ut, Avenue;San Juan;80\n' +
+  'Olsen;Kevin;994-8124 Neque. Road;Fulton;38\n' +
+  'House;Justin;4557 Ligula. Rd.;Mount Vernon;22\n' +
+  'Sloan;Adara;P.O. Box 351, 8153 Molestie Ave;Bowling Green;22\n' +
+  'Rowland;Stephen;4539 Interdum Rd.;Carbondale;88\n' +
+  'Rodgers;Jocelyn;3857 Aliquam Rd.;Attleboro;28\n' +
+  'Page;Maxine;P.O. Box 439, 129 Id, St.;East St. Louis;77\n' +
+  'Pate;Eaton;435-3253 Nam Rd.;Eden Prairie;13\n' +
+  'Tyler;Aimee;3759 Consectetuer, St.;Marietta;87\n' +
+  'Whitfield;Gage;Ap #135-6634 Ligula St.;Salt Lake City;18\n' +
+  'Travis;Cara;P.O. Box 908, 4472 Ac St.;Cedar City;38\n' +
+  'Schwartz;Karleigh;Ap #682-1102 Pede St.;Dover;63\n' +
+  'Bradford;Lucas;Ap #606-3982 In St.;Monterey;19\n' +
+  'Holden;Danielle;P.O. Box 231, 8154 Dolor Rd.;Fort Dodge;15\n' +
+  'Payne;Patrick;Ap #836-8682 Massa. St.;Oklahoma City;43\n' +
+  'Lambert;Piper;3230 Semper St.;Joplin;82\n' +
+  'Sweeney;Zoe;P.O. Box 753, 7088 Iaculis Rd.;Columbus;32\n' +
+  'Skinner;Gage;885-9007 Praesent St.;San Marino;1\n' +
+  'Jarvis;Lyle;P.O. Box 445, 8845 Sit St.;San Juan;54\n' +
+  'Pope;Blaze;359 Tincidunt Rd.;Vincennes;42\n' +
+  'Day;Maxwell;P.O. Box 118, 7563 Congue Road;Chickasha;16\n' +
+  'Dorsey;Allen;P.O. Box 591, 6607 A St.;Twin Falls;61\n' +
+  'Prince;Elmo;2963 Dictum Rd.;North Pole;83\n' +
+  'Vega;Mariko;P.O. Box 251, 5133 Nunc Av.;Eugene;53\n' +
+  'Knox;Emery;629-7119 Rhoncus. Ave;Memphis;57\n' +
+  'Ramos;Beau;406 Ornare, Rd.;Webster Groves;30\n' +
+  'Martin;Savannah;Ap #583-8519 Bibendum St.;Pawtucket;36\n' +
+  'Weiss;Sierra;468-5356 Consequat, Street;San Angelo;8\n' +
+  'Adams;Hakeem;P.O. Box 585, 6497 Dui, Rd.;Fernley;18\n' +
+  'Hernandez;Fleur;9790 Suspendisse Ave;Great Falls;9\n' +
+  'Pickett;Tallulah;915-2792 Pede. Road;Clarksville;13\n' +
+  'Pena;Leila;Ap #707-8727 Ultricies Rd.;La Habra;21\n' +
+  'Stevenson;Aaron;P.O. Box 357, 4990 Erat St.;Gloversville;3\n' +
+  'Hanson;Ferris;P.O. Box 334, 4388 Vivamus Rd.;La Habra Heights;49\n' +
+  'Gill;Orlando;371-1581 Dignissim Rd.;Webster Groves;88\n' +
+  'Schroeder;Rudyard;Ap #488-9240 Eleifend, St.;Steubenville;57\n' +
+  'Crane;Gloria;Ap #359-6106 Mauris Street;Del Rio;31\n' +
+  'Stanley;Megan;5353 Duis Road;Burlingame;44\n' +
+  'Winters;Igor;506-7504 Venenatis Avenue;Selma;46\n' +
+  'Horn;Demetria;6390 Laoreet Street;Oro Valley;77\n' +
+  'Stanton;Hop;Ap #530-3494 Cursus Rd.;Flint;25\n' +
+  'Randall;Emerald;7877 Ultricies Av.;Fernley;37\n' +
+  'Pearson;Myra;Ap #420-9062 Fringilla Rd.;Ruston;71\n' +
+  'Cruz;Maxine;Ap #357-2987 Magna. Av.;Ocean City;69\n' +
+  'Villarreal;Brittany;868-5257 Nunc Street;Cudahy;66\n' +
+  'Flores;Zephr;Ap #735-5552 Sollicitudin Ave;Citrus Heights;20\n' +
+  'Trujillo;Audrey;676-1161 Dolor Ave;Citrus Heights;14\n' +
+  'Harrison;Jane;935-2659 Mi St.;New Iberia;30\n' +
+  'Buchanan;Buffy;584-8562 Eleifend Av.;Cambridge;23\n' +
+  'Manning;Molly;Ap #544-7679 Magna. Av.;Eugene;71\n' +
+  'Henderson;Harriet;4240 Euismod Rd.;Greensburg;84\n' +
+  'Salinas;Ebony;Ap #372-5684 Nulla Road;Chesapeake;13\n' +
+  'York;Nehru;5589 Posuere, St.;Santa Cruz;33\n' +
+  'Gentry;Hedda;Ap #675-5099 Purus. St.;Bangor;54\n' +
+  'Benjamin;Sarah;8246 Duis Road;Phoenix;86\n' +
+  'Douglas;Fallon;456-8118 Diam Av.;Christiansted;36\n' +
+  'Waller;Blythe;350-1452 Nibh Ave;Gardner;34\n' +
+  'Salas;Xander;Ap #947-6985 Ultricies Av.;Modesto;33\n' +
+  'Hunter;Hedy;Ap #431-6763 Eu, Road;La Cañada Flintridge;76\n' +
+  'Best;Dominic;P.O. Box 205, 4108 Aliquam Rd.;Webster Groves;53\n' +
+  'Odonnell;Ayanna;8813 Ornare St.;Waterbury;68\n' +
+  'Hoover;Sylvester;P.O. Box 919, 4168 Eu St.;Fernley;30\n' +
+  'Beck;Melinda;Ap #161-7870 Dui Rd.;Longview;48\n' +
+  'Workman;Magee;P.O. Box 654, 324 Adipiscing Avenue;Calumet City;61\n' +
+  'Valencia;Adara;550-1291 Aliquet, Ave;La Mirada;67\n' +
+  'Baker;Steven;7003 Eu Road;Greenville;41\n' +
+  'Cooke;Iliana;2298 Cubilia Ave;Gettysburg;80\n' +
+  'Chambers;Kiayada;Ap #261-1067 Vehicula St.;Pascagoula;72\n' +
+  'Richardson;Brooke;Ap #135-9197 Justo Av.;Laguna Beach;7\n' +
+  'Everett;Herrod;9706 Convallis Rd.;Barre;8\n' +
+  'Nunez;Adele;4673 Massa. St.;Miami;37\n' +
+  'Morris;Dale;3836 Id, Ave;Los Alamitos;5\n' +
+  'Norris;Helen;202-7906 Natoque Ave;Muncie;47\n' +
+  'Mann;Cruz;498-2159 Diam. Rd.;Farmer City;67\n' +
+  'Dalton;Charlotte;Ap #866-199 Molestie Ave;Christiansted;54\n' +
+  'Velazquez;Kevyn;P.O. Box 551, 6222 Nunc Avenue;Walla Walla;74\n' +
+  'Pope;Oleg;673-6141 Sed Street;Savannah;83\n' +
+  'Beach;Alana;P.O. Box 456, 136 Ligula. Av.;Altoona;89';
